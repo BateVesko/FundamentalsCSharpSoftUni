fee824353fa41e7d5ac814732e8b6598542a9f17
BateVesko <133985940+BateVesko@users.noreply.github.com>
2023-05-23 11:33:24 +0300

    Add new code in IF operator

---
+++ Solution.cs
@@ -0,0 +1,12 @@
+using System;
+
+int number=int.Parse(Console.ReadLine());   
+Console.WriteLine(number);
+if (number < 0)
+{
+    Console.WriteLine("0");
+}
+else
+{
+    Console.WriteLine("The number is positiv.");
+}
